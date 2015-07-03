aeea8789339cf86dbfd200a9ce0b0cdbeb69cce9
Yvonne <ngdotyvonne@gmail.com>
2015-07-03 11:36:02 +0800

    Main user node

--- instalkr/SearchViewController.swift
+++ instalkr/SearchViewController.swift
@@ -14,10 +14,9 @@ class SearchViewController : UIViewController
 
     @IBOutlet weak var relationshipScrollView: UIScrollView!
 
-    @IBOutlet weak var mainUserNode: VMUser!
-    
+    var mainUserNode : VMUser?
     var theGraph : Graph
-    
+
     
     required init(coder aDecoder: NSCoder)
     {
@@ -27,7 +26,9 @@ class SearchViewController : UIViewController
         var userPref = NSUserDefaults.standardUserDefaults()
         
         var mainUser : UserNode = UserNode(me: Model_User.createMUser(userPref.objectForKey("user")!))
+        
         self.theGraph = Graph(theMainUser: mainUser)
+        
         
         super.init(coder: aDecoder)
     }
@@ -42,8 +43,143 @@ class SearchViewController : UIViewController
     {
         
         super.viewDidLoad()
+        self.createUserView( true )
+        self.reloadUserView(self.theGraph.userInFocus.myself, userView: mainUserNode!)
         
-        self.mainUserNode.showView( self.theGraph.userInFocus.myself )
+    }
+    
+    func createUserView ( main : Bool )
+    {
+        var frame : CGRect
+        var x : CGFloat
+        var y : CGFloat
+        var widthHeight : CGFloat
+        
+        
+        
+        if ( main )
+        {
+            widthHeight = 200
+            x = (relationshipScrollView.bounds.width - widthHeight)/2
+            y = (relationshipScrollView.bounds.height - widthHeight)/2
+            
+        }
+        else
+        {
+            widthHeight = 160
+            x = (relationshipScrollView.bounds.width - widthHeight)/2
+            y  = (relationshipScrollView.bounds.height - widthHeight)/2
+            
+        }
+        frame = CGRectMake(x, y, widthHeight, widthHeight)
+        mainUserNode = VMUser(frame: frame)
+
+        x = frame.width / 20
+        var imgWidth : CGFloat = frame.width - (x * 2)
+        
+        mainUserNode!.imageView = UIImageView(frame: CGRectMake( x, 0, imgWidth, imgWidth))
+        mainUserNode!.usernameLabel = UILabel(frame: CGRectMake( x, imgWidth, imgWidth, x * 2))
+
+        
+     
+        mainUserNode!.addSubview(mainUserNode!.imageView!)
+        mainUserNode!.addSubview(mainUserNode!.usernameLabel!)
+        
+        self.relationshipScrollView.addSubview(mainUserNode!)
+        
+        
+        // to make the UIImageView circular
+        mainUserNode!.imageView!.layer.cornerRadius = mainUserNode!.imageView!.frame.size.height/2
+        mainUserNode!.imageView!.layer.masksToBounds = true
+        mainUserNode!.imageView!.layer.borderColor = UIColor.lightGrayColor().CGColor
+        mainUserNode!.imageView!.layer.borderWidth = 0.7
+        
+        mainUserNode!.usernameLabel!.font = UIFont(name: "Helvetica Neue", size: 16.0)
+        mainUserNode!.usernameLabel!.textAlignment = NSTextAlignment.Center
+        mainUserNode!.usernameLabel!.textColor = UIColor.grayColor()
+
+        
+   // Add constraints  -->   item1.attribute1 = multiplier × item2.attribute2 + constant
+        
+   //  Set width & height of View
+        
+        
+       mainUserNode!.addConstraint(NSLayoutConstraint(item: mainUserNode!, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1.0, constant: 200))
+        
+       mainUserNode!.addConstraint(NSLayoutConstraint(item: mainUserNode!, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1.0, constant: 200))
+       
+   //  Set center of margin with regards to the superview
+        
+        relationshipScrollView.addConstraint(NSLayoutConstraint(item: mainUserNode!, attribute: NSLayoutAttribute.CenterXWithinMargins, relatedBy: NSLayoutRelation.Equal, toItem: relationshipScrollView, attribute: NSLayoutAttribute.CenterXWithinMargins, multiplier: 1.0, constant: 0))
+        
+        relationshipScrollView.addConstraint(NSLayoutConstraint(item: mainUserNode!, attribute: NSLayoutAttribute.CenterYWithinMargins, relatedBy: NSLayoutRelation.Equal, toItem: relationshipScrollView, attribute: NSLayoutAttribute.CenterYWithinMargins, multiplier: 1.0, constant: 0))
+        
+        
+ // --> This is the damn piece I was missing all along! -.-
+        mainUserNode!.setTranslatesAutoresizingMaskIntoConstraints(false)
+
+        
+    }
+    
+    
+    func reloadUserView( user : Model_User, userView : VMUser )
+    {
+        var theSession = NSURLSession.sharedSession()
+        
+    
+        
+        if( theGraph.userInFocus.myself.id == user.id )
+        {
+            
+        }
+        else
+        {
+            
+        }
+        
+        
+        
+        
+
+
+
+        
+        
+        // Get Image
+     
+        if let url = NSURL( string: user.profile_picture )
+        {
+            var task : NSURLSessionDataTask =  theSession.dataTaskWithURL(url, completionHandler: {
+                
+                (data, response, error) -> Void in
+                dispatch_async(dispatch_get_main_queue(),{
+                    self.mainUserNode!.imageView!.image = UIImage(data: data)!
+                    
+                    UIView.animateWithDuration(1.0, delay: 0.8, options: UIViewAnimationOptions.CurveLinear, animations: {   () -> Void in
+                        
+                        userView.usernameLabel!.text = user.username
+                        userView.usernameLabel!.alpha = 1.0
+                        
+                        }
+                        , completion: {   (done) -> Void in
+                            
+                            UIView.animateWithDuration(0.5, animations: {   () -> Void in
+                                
+                                userView.usernameLabel!.font = UIFont(name: "Helvetica Neue Light", size: 15.0)
+                                
+                            })
+                    })
+                    
+                    return
+                    
+                })
+                
+                
+                
+            })
+            
+            task.resume()
+        }
         
         
     }
--- instalkr/ViewModel_User.swift
+++ instalkr/ViewModel_User.swift
@@ -2,7 +2,7 @@
 //  ViewModel_User.swift
 //  instalkr
 //
-//  Created by Yvonne on 26/6/15.
+//  Created by Yvonne Ng on 7/2/15.
 //  Copyright (c) 2015 sassyCodes. All rights reserved.
 //
 
@@ -11,108 +11,16 @@ import UIKit
 
 class VMUser : UIView
 {
-    var imageView : UIImageView
-    var usernameLabel : UILabel
+    var imageView : UIImageView?
+    var usernameLabel : UILabel?
     
-    
-    required override init(frame: CGRect)
-    {
-        
-        self.imageView = UIImageView(frame: CGRectMake(10, 179, 180, 21))
-        self.usernameLabel = UILabel(frame: CGRectMake(10, 0, 180, 180))
-        
-        super.init(frame: frame)
-        
-        
-        self.addSubview(self.imageView)
-        self.addSubview(self.usernameLabel)
-        
-        
-    }
-
     required init(coder aDecoder: NSCoder)
     {
-        
-        self.imageView = UIImageView(coder: aDecoder)
-        self.usernameLabel = UILabel(coder: aDecoder)
-        
         super.init(coder: aDecoder)
-        
-        
-        self.addSubview(self.imageView)
-        self.addSubview(self.usernameLabel)
-        
     }
     
-    
-    func showView( user : Model_User )
+    override init(frame: CGRect)
     {
-        
-        
-        self.imageView.layer.cornerRadius = 5.0
-        self.imageView.layer.masksToBounds = true
-        self.imageView.layer.borderColor = UIColor.blueColor().CGColor
-        self.imageView.layer.borderWidth = 1.0
-        
-        self.usernameLabel.font = UIFont(name: "Helvetica Neue", size: 16.0)
-        self.usernameLabel.textAlignment = NSTextAlignment.Center
-        self.usernameLabel.textColor = UIColor.darkGrayColor()
-        var theSession = NSURLSession.sharedSession()
-
-        // Show username first
-        self.usernameLabel.text = user.username
-        
-        // Get Image
-/*
-        if let url = NSURL( string: user.profile_picture )
-        {
-            theSession.dataTaskWithURL(url, completionHandler: {
-                
-                (data, response, error) -> Void in
-                
-                self.imageView.image = UIImage(data: data)!
-                
-            }).resume()
-        }
-*/
-        
-        /*
-        [session downloadTaskWithURL:[NSURL URLWithString:imageUrl]
-        
-        completionHandler:^(NSURL *location,
-        NSURLResponse *response,
-        NSError *error) {
-        // 2
-        UIImage *downloadedImage =
-        [UIImage imageWithData:
-        [NSData dataWithContentsOfURL:location]];
-        //3
-        dispatch_async(dispatch_get_main_queue(), ^{
-        // do stuff with image
-        _imageWithBlock.image = downloadedImage;
-        });
-        }];
-        */
-        
-        
-        if let url = NSURL( string: user.profile_picture )
-        {
-            theSession.downloadTaskWithURL(url, completionHandler: {
-                
-                (location, response, error) -> Void in
-                
-                var downloadedImg : UIImage = UIImage(data: NSData(contentsOfURL: location)!)!
-                
-                dispatch_async(dispatch_get_main_queue(),{
-                    self.imageView.image = downloadedImg
-                })
-            })
-        }
-        
-        
-        
-        
-        
-
+       super.init(frame: frame)
     }
 }
